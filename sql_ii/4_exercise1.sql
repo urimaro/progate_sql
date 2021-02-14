92d773327cdeef133cc2527658f6e09a8984f4e1
Goh Matsumoto <urimaro@gmail.com>
2021-02-14 14:48:35 +0900

    4. AVG - 2. Using Aggregate Functions

---
+++ sql_ii/4_exercise1.sql
@@ -0,0 +1,4 @@
+-- priceカラムの平均を取得してください
+
+SELECT AVG(price)
+FROM purchases;
---
+++ sql_ii/4_exercise2.sql
@@ -0,0 +1,5 @@
+-- character_nameが「にんじゃわんこ」であるpriceカラムのデータの平均を取得してください
+
+SELECT AVG(price)
+FROM purchases
+WHERE character_name = "にんじゃわんこ";
